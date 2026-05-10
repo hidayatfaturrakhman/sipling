261e582eeb64753d383432767605433d0b6f7f4d
Hidayat <hidayat@example.com>
2026-05-10 18:04:31 +0800

    fix: remove cn function from utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,3 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 // Image compression utility
 // Optimized for 1-2MB output with good quality
 
